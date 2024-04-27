e95e119dbabee0777db9f1851b1b462542e2e2e2
Kanchan Dixit <144261891+kanchan2803@users.noreply.github.com>
2024-04-27 20:53:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pytesseract
 import cv2
 # from click import capture_image
-pytesseract.pytesseract.tesseract_cmd = r'/usr/local/bin/pytesseract'
+pytesseract.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
+
 # pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
 myconfig = r"--psm 11 --oem 3"
